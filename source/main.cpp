51e28107dd478ec2e75e4c46ff826bf31dfe57f0
Zerofo <46804648+zerofo@users.noreply.github.com>
2024-08-05 00:59:03 +0800

    Oled Support

--- source/main.cpp
+++ source/main.cpp
@@ -120,13 +120,8 @@ public:
             return initially<FastCFWSwitchErrorGui>("Failed to get hardware type\nError code: "+std::to_string(rc));
         }
         const bool is_erista = hardware_type == 0 || hardware_type == 1;
-        if(!is_erista) {
-            // unsupported Switch model, show error
-            return initially<FastCFWSwitchErrorGui>("This Switch model is not supported\n\nReboot to payload is only possible\non an Erista Switch");
-        } else {
             // create main GUI with payload selection
-            return initially<FastCFWSwitchGui>(useClassic);
-        }
+        return initially<FastCFWSwitchGui>(useClassic);
     }
 };
 
